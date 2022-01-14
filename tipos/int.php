0801aa0d9c262b7b433bec821489db8fbce5f1b4
jhonisrael <jhonatafarias2930@gmail.com>
2022-01-14 00:01:12 -0300

    nova linha int

--- tipos/int.php
+++ tipos/int.php
@@ -1,4 +1,4 @@
-<div class="titulo">tipo inteiro</div>
+﻿<div class="titulo">tipo inteiro</div>
 
 <?php
 echo  11;
@@ -12,3 +12,4 @@ echo PHP_INT_MIN, '<br>';
 echo 017, '<br>'; //base octal, que vai de 0 a 7
 echo 0b0110000, '<br>'; //base binaria, que vai de 0 a 1
 echo 0x178abdf, '<br>'; //base exadecimal, que vai de 0 a 9 e até a letra F
+echo PHP_INT_MAX;
